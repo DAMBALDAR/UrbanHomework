c96ecaed432ad7a5e88165cba07f8c8096483b8f
Алдар Дамбаев <aldar.dambaev@mail.ru>
2024-09-07 23:54:27 +0800

    Added module_1_7.py

---
+++ module_1_7.py
@@ -0,0 +1,20 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+# list с суммами и list числом элементов для grades
+sum_list =  list(map(sum, grades))
+len_list = list(map(len, grades))
+
+
+
+# set в list и сортировка
+students_list = list(students)
+students_list.sort()
+# или students_list = sorted(students)
+
+# собираю два list в один dict
+average_score = dict(zip(students_list, grades))
+print(average_score)
+
+#print(list(map(average_score.get,[*students_list])))
+#способ достать список значений обращаясь в словарь по элементам ключа.
