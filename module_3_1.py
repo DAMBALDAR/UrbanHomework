1b93731e593b479f6f7d124831105619b7f56302
Алдар Дамбаев <aldar.dambaev@mail.ru>
2024-09-20 20:50:55 +0900

    Added module_3_1.py

---
+++ module_3_1.py
@@ -0,0 +1,33 @@
+calls = 0
+
+# Функция count_calls подсчитывающая вызовы остальных функций.
+def count_calls():
+    global calls
+    calls += 1
+
+# Функция string_info принимает аргумент - строку и возвращает кортеж из:
+# длины этой строки, строку в верхнем регистре, строку в нижнем регистре.
+def string_info(string):
+    count_calls()
+    result = (len(string), string.lower(), string.upper())
+    return result
+
+
+# Функция is_contains принимает два аргумента: строку и список, и возвращает True,
+# если строка находится в этом списке, False - если отсутствует. Регистром строки
+# при проверке пренебречь: UrbaN ~ URBAN.
+def is_contains(string, li_st):
+    count_calls()
+    result = False
+    for i in li_st:
+        if i.lower() == string.lower():
+            result = True
+    return result
+
+"""
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
+"""
